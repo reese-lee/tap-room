cb184a9466e528cd7665cf8f5718835710c54557
Lindsey Baker and Reese Lee <reese@>
2019-06-13 09:44:14 -0700

    Add TapRoom constructor

---
+++ src/app/models/tap-room.model.ts
@@ -0,0 +1,4 @@
+export class TapRoom {
+  public pintsRemaining: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public ABV: number, public flavorNotes: string) { }
+}
