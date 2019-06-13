ea6c036fa524b53f5bbfb41f888f32a6e335cfdf
Lindsey Baker and Reese Lee <reese@>
2019-06-13 09:44:58 -0700

    Rename object TapRoom to Beer

--- src/app/models/tap-room.model.ts
+++ src/app/models/tap-room.model.ts
@@ -1,4 +1,4 @@
-export class TapRoom {
+export class Beer {
   public pintsRemaining: number = 124;
   constructor(public name: string, public brand: string, public price: number, public ABV: number, public flavorNotes: string) { }
 }
